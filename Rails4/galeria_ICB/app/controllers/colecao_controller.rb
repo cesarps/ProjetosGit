1bcb2a566e0f437b0725004e6daa322dc2a2eaf2
cesarps <cps2005@gmail.com>
2018-09-11 15:42:09 -0300

    busca por legenda concluída, incluindo uma busca casada.

--- Rails4/galeria_ICB/app/controllers/colecao_controller.rb
+++ Rails4/galeria_ICB/app/controllers/colecao_controller.rb
@@ -11,36 +11,49 @@ class ColecaoController < ApplicationController
     @fotos = Foto.where(album_id: @album.id).where(categoria_id: 2)
   end
 
+
+
+
   def pesquisa
     busca = ""
 
     if params[:data_inicio] != "" and params[:data_fim] != ""
-      busca = "data_evento between '" + params[:data_inicio] + "' and '" + params[:data_fim] + "'"
+      busca = "data_evento between '" + params[:data_inicio] + "' and '" + params[:data_fim] + "'"		# busca pela data
     end
 
     if params[:nome_evento] != ""
       if busca != ""
         busca = busca + " and "
       end
-      busca = busca + "nome_evento_assunto like '%" + params[:nome_evento] + "%'"
+      busca = busca + "nome_evento_assunto like '%" + params[:nome_evento] + "%'"				# busca pelo evento
     end
 
     if !params[:departamento_id].empty?
       if busca != ""
         busca = busca + " and "
       end
-      busca = busca + " departamento_id = " + params[:departamento_id].to_s
+      busca = busca + " departamento_id = " + params[:departamento_id].to_s					# busca pelo departamento
     end
 
     if busca != ""
       @album = Album.find_by_sql("select * from albuns where " +  busca)
-      respond_to do |format|
-        format.html
+      if params[:legenda] != ""
+        @album = Album.joins(:fotos).where("fotos.legenda like ? ", "%"+params[:legenda]+"%")			# busca pela legenda somente se ela for preenchida
+        respond_to do |format|
+          format.html
+        end
       end
+    elsif params[:legenda] != ""                                                                # somente params[:legenda]
+        @album = Album.joins(:fotos).where("fotos.legenda like ? ", "%"+params[:legenda]+"%")			# busca pela legenda somente se ela for preenchida
+        respond_to do |format|
+          format.html
+        end
     else
-      redirect_to colecao_index_path
+        redirect_to colecao_index_path
     end
   end
+
+
 
 
   def busca_tag
