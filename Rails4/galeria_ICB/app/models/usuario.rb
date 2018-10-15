205cc9bbff8088471c92520f58b14285c9506035
cesarps <cps2005@gmail.com>
2018-10-15 11:00:30 -0300

    Ao apagar usuario, tambem vai apagar suas permissoes

--- Rails4/galeria_ICB/app/models/usuario.rb
+++ Rails4/galeria_ICB/app/models/usuario.rb
@@ -2,6 +2,6 @@ class Usuario < ActiveRecord::Base
   has_many :tipo_vinculos
   has_many :logs
 
-  has_many :permitidos
+  has_many :permitidos, dependent: :destroy
   has_many :perfils, through: :permitidos
 end
